e662ef7fb5e73e1dd87a9c662dc078b23fe1344b
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-20 00:36:19 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector(".gallery");
+
+const gallery = galleryItems
+  .map(
+    (img) =>
+      `<a class="gallery__item" href="${img.original}"><img class="gallery__image" src="${img.preview}" data-source="${img.original}" alt="${img.description}"/></a>`
+  )
+  .join("");
+
+galleryDiv.innerHTML = gallery;
+
+galleryDiv.addEventListener("click", openImg);
+
+function openImg(event) {
+  event.preventDefault();
+  console.log(event.target.alt);
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  lightbox.open();
+}
