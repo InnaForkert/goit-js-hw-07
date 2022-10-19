63b7054d321096dc8d1757c9fe5e39fc01affa62
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-20 00:01:55 +0300

    gallery without working escape btn

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// Створення і рендер розмітки на підставі масиву даних
+const galleryDiv = document.querySelector(".gallery");
+
+const gallery = galleryItems
+  .map(
+    (img) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${img.original}"><img class="gallery__image" src="${img.preview}" data-source="${img.original}" alt="${img.description}"/></a></div>`
+  )
+  .join("");
+
+galleryDiv.innerHTML = gallery;
+
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+
+galleryDiv.addEventListener("click", openImg);
+
+function openImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName === "DIV") {
+    return;
+  }
+  // модалка
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src=${event.target.dataset.source} width="80%" style="margin: auto; display: block"> 
+    </div>
+`);
+  instance.show();
+  modalCheck();
+}
+
+function closeModal(event) {
+  console.log("l");
+  if (event.key === "Escape") {
+    instance.close();
+    document.removeEventListener("keydown", closeModal);
+  }
+}
+function modalCheck() {
+  document.addEventListener("keydown", closeModal;
+}
+
+https: console.log(galleryItems);
