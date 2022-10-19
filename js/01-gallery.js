b82ad850431841505adc302fd8e305e9bc930bac
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-20 00:09:42 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// Створення і рендер розмітки на підставі масиву даних
 const galleryDiv = document.querySelector(".gallery");
 
 const gallery = galleryItems
@@ -13,34 +12,29 @@ const gallery = galleryItems
 
 galleryDiv.innerHTML = gallery;
 
-// Реалізація делегування на div.gallery і отримання url великого зображення.
-
 galleryDiv.addEventListener("click", openImg);
+
+let instance;
 
 function openImg(event) {
   event.preventDefault();
   if (event.target.nodeName === "DIV") {
     return;
   }
-  // модалка
-  const instance = basicLightbox.create(`
+  instance = basicLightbox.create(`
     <div class="modal">
         <img src=${event.target.dataset.source} width="80%" style="margin: auto; display: block"> 
     </div>
 `);
   instance.show();
-  modalCheck();
+  document.addEventListener("keydown", closeModal);
 }
 
 function closeModal(event) {
-  console.log("l");
   if (event.key === "Escape") {
     instance.close();
     document.removeEventListener("keydown", closeModal);
   }
-}
-function modalCheck() {
-  document.addEventListener("keydown", closeModal;
 }
 
 https: console.log(galleryItems);
